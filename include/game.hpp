56c98ded181eadd8add5405544a857bc817053a3
Lenny Garnier <lenny.garnier@epitech.eu>
2023-02-04 15:48:32 +0400

    added buttons

--- include/game.hpp
+++ include/game.hpp
@@ -2,10 +2,121 @@
 
 #include "include.hpp"
 #include "window.hpp"
+#include <functional>
 
 #pragma once
 
 using namespace std;
+
+class button {
+    private:
+    public:
+    virtual void effect(void) = 0;
+
+};
+
+class diaryButton : public button {
+    public:
+    void effect() override {
+        cout << "Hello\n";
+    }
+
+    void initButtonRectangleShape(sf::Vector2f position,
+        sf::Color fillColor, float outlineThickness, sf::Color outlineColor,
+        sf::Vector2f size)
+    {
+        rect.setPosition(position);
+        rect.setFillColor(fillColor);
+        rect.setOutlineThickness(outlineThickness);
+        rect.setOutlineColor(outlineColor);
+        rect.setSize(size);
+
+        hitBox.width = size.x;
+        hitBox.height = size.y;
+        hitBox.top = position.y;
+        hitBox.left = position.x;
+        return;
+    }
+
+    void drawButton(sf::RenderWindow *window)
+    {
+        window->draw(rect);
+        window->draw(text);
+    }
+
+    int isMouseOnButton(sf::Vector2f mousePos)
+    {
+        if (hitBox.contains(mousePos) == true &&
+            sf::Mouse::isButtonPressed(sf::Mouse::Left) == true &&
+            isMouseClicked == true) {
+                return 0;
+            this->effect();
+            isMouseClicked = true;
+            return 1;
+        }
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left) == false)
+            isMouseClicked = false;
+        return 0;
+    }
+
+    bool isMouseClicked = false;
+    sf::FloatRect hitBox;
+    sf::Vector2f position;
+    sf::Font font;
+    sf::Text text;
+    sf::RectangleShape rect;
+};
+
+class treeButton : public button {
+    public:
+    void effect () override {
+        cout << "Tree\n";
+    }
+
+    void initButtonRectangleShape(sf::Vector2f position,
+        sf::Color fillColor, float outlineThickness, sf::Color outlineColor,
+        sf::Vector2f size)
+    {
+        rect.setPosition(position);
+        rect.setFillColor(fillColor);
+        rect.setOutlineThickness(outlineThickness);
+        rect.setOutlineColor(outlineColor);
+        rect.setSize(size);
+
+        hitBox.width = size.x;
+        hitBox.height = size.y;
+        hitBox.top = position.y;
+        hitBox.left = position.x;
+        return;
+    }
+
+    void drawButton(sf::RenderWindow *window)
+    {
+        window->draw(rect);
+        window->draw(text);
+    }
+
+    int isMouseOnButton(sf::Vector2f mousePos)
+    {
+        if (hitBox.contains(mousePos) == true &&
+            sf::Mouse::isButtonPressed(sf::Mouse::Left) == true &&
+            isMouseClicked == false) {
+            effect();
+            isMouseClicked = true;
+            return 1;
+        }
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left) == false)
+            isMouseClicked = false;
+        return 0;
+    }
+
+    bool isMouseClicked = false;
+    sf::FloatRect hitBox;
+    sf::Vector2f position;
+    sf::Font font;
+    sf::Text text;
+    sf::RectangleShape rect;
+};
 
 class game
 {
@@ -13,9 +124,12 @@ class game
         windowClass win;
     public:
         static game *instance;
+        diaryButton diaryBut;
+        treeButton treeBut;
         std::vector<std::shared_ptr<player>> players;
         vector<sf::Text> dialog;
         sf::RectangleShape dialogPannel;
+
         sf::Font font;
         
         game();
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,53 @@
 #include "game.hpp"
 
+void setText(sf::Text *text, sf::Font *font, string fontPath,
+    string content, float charSize, sf::Vector2f position, sf::Color fillColor)
+{
+    font->loadFromFile(fontPath);
+    text->setFont((*font));
+    text->setFillColor(fillColor);
+    text->setCharacterSize(charSize);
+    text->setString(content);
+    text->setPosition(position);
+    return;
+}
+
+void setupDiaryButton(game **gm)
+{
+    (*gm)->diaryBut.position = (sf::Vector2f){1680, 40};
+    (*gm)->diaryBut.initButtonRectangleShape((*gm)->diaryBut.position,
+        sf::Color::Black, 4, sf::Color::White, (sf::Vector2f){200, 70});
+
+    setText(&(*gm)->diaryBut.text, &(*gm)->diaryBut.font, "./assets/fonts/arial.ttf",
+        "DIARY", 50, (sf::Vector2f){(*gm)->diaryBut.position.x + 50, (*gm)->diaryBut.position.y + 5}, sf::Color::White);
+    return;
+}
+
+void setupTreeButton(game **gm)
+{
+    (*gm)->treeBut.position = (sf::Vector2f){1440, 40};
+    (*gm)->treeBut.initButtonRectangleShape((*gm)->treeBut.position,
+        sf::Color::Black, 4, sf::Color::White, (sf::Vector2f){200, 70});
+
+    setText(&(*gm)->treeBut.text, &(*gm)->treeBut.font, "./assets/fonts/arial.ttf",
+        "TREE", 50, (sf::Vector2f){(*gm)->treeBut.position.x + 50, (*gm)->treeBut.position.y + 5}, sf::Color::White);
+    return;
+}
+
 int main(void)
 {
     game *gm = game::getInstance();
     sf::Event event;
     int person = 0;
     int isPressed = 0;
+    sf::Vector2i mouse;
 
     gm->loadPlayers("./ressources/characters.json");
     gm->getWindow()->createWindow(1920, 1080, "name");
     dialog(&gm, person);
+
+    setupDiaryButton(&gm);
+    setupTreeButton(&gm);
 
     gm->dialogPannel = initRectangleShape((sf::Vector2f){4, 707},
         sf::Color::Black, 4, sf::Color::White, (sf::Vector2f){1910, 300});
@@ -17,6 +55,11 @@ int main(void)
     while (gm->getWindow()->getWindow()->isOpen()) {
         gm->getWindow()->getWindow()->clear();
         gm->getWindow()->getWindow()->draw(gm->dialogPannel);
+        gm->diaryBut.drawButton(gm->getWindow()->getWindow());
+        gm->treeBut.drawButton(gm->getWindow()->getWindow());
+        mouse = sf::Mouse::getPosition((*gm->getWindow()->getWindow()));
+        gm->diaryBut.isMouseOnButton((sf::Vector2f){(float)mouse.x, (float)mouse.y});
+        gm->treeBut.isMouseOnButton((sf::Vector2f){(float)mouse.x, (float)mouse.y});
         for (size_t i = 0; i < gm->dialog.size(); i++)
             gm->getWindow()->getWindow()->draw(gm->dialog[i]);
         gm->getWindow()->getWindow()->display();
