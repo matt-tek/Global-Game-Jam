f20407a863ea8682e1de668761f542ee4b329c91
ludovicplt <ludovic.peltier@epitech.eu>
2023-02-05 16:53:03 +0400

    adding final push fullscreen + accent

--- src/window.cpp
+++ src/window.cpp
@@ -11,7 +11,7 @@ void windowClass::createWindow(int width, int height, std::string window_name)
 {
     mode.width = width;
     mode.height = height;
-    window.create(sf::VideoMode::getDesktopMode(), window_name);
+    window.create(sf::VideoMode::getDesktopMode(), window_name, sf::Style::Fullscreen);
     return;
 }
 
